9aa14222161eedbf4e2d7e84634b10ea90009745
Gleb Nasretdinov <gleb.romashkov@gmail.com>
2024-07-18 12:02:16 +0300

    feat: add global variables definition and floats detection

--- src/Postprocessor.py
+++ src/Postprocessor.py
@@ -6,8 +6,9 @@ pyhidra.start()
 
 from ghidra.app.decompiler import DecompInterface
 from ghidra.util.task import ConsoleTaskMonitor
+from ghidra.program.model.address import AddressRangeImpl
 
-undefined_types = {
+integer_types = {
     "undefined1": "char",
     "undefined2": "short",
     "undefined3": "int",
@@ -17,6 +18,12 @@ undefined_types = {
     "undefined7": "long long",
     "undefined8": "long long",
     "undefined": "char",
+}
+
+float_types = {
+    "undefined4": "double",
+    "undefined8": "double",
+    "undefined": "double"
 }
 
 static_linked_funcs = ["_init", "_start", "deregister_tm_clones", "register_tm_clones",
@@ -33,9 +40,9 @@ utypes = {
 }
 
 libc = ["assert.h", "ctype.h", "complex.h", "errno.h", "fenv.h", "float.h", "inttypes.h",
-                      "iso646.h", "limits.h", "locale.h", "math.h", "setjmp.h", "signal.h", "stdarg.h",
-                      "stdbool.h", "stdint.h", "stddef.h", "stdio.h", "stdlib.h", "string.h", "tgmath.h",
-                      "threads.h", "time.h", "wchar.h", "wctype.h"]
+        "iso646.h", "limits.h", "locale.h", "math.h", "setjmp.h", "signal.h", "stdarg.h",
+        "stdbool.h", "stdint.h", "stddef.h", "stdio.h", "stdlib.h", "string.h", "tgmath.h",
+        "threads.h", "time.h", "wchar.h", "wctype.h"]
 
 types_from_libc = {
     "bool": "stdbool.h",
@@ -53,6 +60,13 @@ types_from_libc = {
     "fexcept_t": "fenv.h",
 }
 
+floating_point_instructions = ["MOVSS", "MOVSD", "ADDSS", "ADDSD", "SUBSS", "SUBSD", "MULSS", "MULSD",
+                               "DIVSS", "DIVSD", "MINSS", "MINSD", "MAXSS", "MAXSD", "SQRTSS", "SQRTSD",
+                               "RCPSS", "RCPSD", "RSQRTSS", "RSQRTSD", "CMPSS", "CMPSD", "CMPEQSS", "CMPEQSD",
+                               "CMPLTSS", "CMPLTSD", "CMPLESS", "CMPLESD", "CMPNESS", "CMPNESD", "CMPUNORDSS",
+                               "CMPUNORDSD", "CMPNLTSS", "CMPNLTSD", "CMPNLESS", "CMPNLESD", "CMPORDSS", "CMPORDSD"]
+
+
 class PostProcessor:
     def __init__(self, filepath: str):
         self.filepath = filepath
@@ -68,8 +82,99 @@ class PostProcessor:
 
             with open(self.filepath + ".c", "w") as file:
                 self.write_headers(file, program, decompiled_funcs)
+                self.write_global_variables(file, flat_api)
                 self.write_funcs(file, filtered_funcs, decompiled_funcs)
         shutil.rmtree(self.filepath + "_ghidra")
+
+    def write_global_variables(self, file, flat_api):
+        sections = (".bss", ".data", ".rodata")
+        for section_name in sections:
+            symbols = self.get_symbols_from_section(flat_api, section_name)
+            for symbol in symbols:
+                result = self.get_symbol_type_and_value(symbol, flat_api)
+                if result is None:
+                    continue
+
+                type, value = result
+                name = symbol.getName()
+                if name[0] == "_" or "std" in name:
+                    continue
+                correct_name = self.correct_variable_name(name)
+                global_var_definition = repr(f"{type} {correct_name} = {value};")[1:-1]
+                file.write(global_var_definition + "\n")
+        file.write("\n")
+
+    def correct_variable_name(self, name: str) -> str:
+        correct_name = ""
+        for index, char in enumerate(name, start=1):
+            if char.isalpha() or char == "_":
+                correct_name += char
+            elif char.isdigit() and index != 0:
+                correct_name += char
+            else:
+                correct_name += "_"
+        return correct_name
+
+    def get_symbols_from_section(self, flat_api, section_name: str):
+        program = flat_api.getCurrentProgram()
+        block = program.getMemory().getBlock(section_name)
+        start_addr = block.getStart()
+        end_addr = block.getEnd()
+        addr_range = AddressRangeImpl(start_addr, end_addr)
+        symbol_table = program.getSymbolTable()
+
+        symbols_in_section = []
+        for addr in addr_range.iterator():
+            symbols_at_addr = symbol_table.getSymbols(addr)
+            if len(symbols_at_addr) == 0:
+                continue
+            symbols_in_section.append(symbol_table.getSymbols(addr)[0])
+        return symbols_in_section
+
+    def get_symbol_type_and_value(self, symbol, flat_api) -> (str, str):
+        program = flat_api.getCurrentProgram()
+        listing = program.getListing()
+        addr = symbol.getAddress()
+        data = listing.getDataAt(addr)
+        type = data.getDataType().getName()
+        value = data.getValue()
+        if value is None:
+            return None
+        if self.is_symbol_float(symbol, program):
+            type = float_types[type]
+            if type == "float":
+                value = flat_api.getFloat(addr)
+            else:
+                value = flat_api.getDouble(addr)
+            return type, value
+        match type:
+            case "pointer":
+                type = "void *"
+                value = int(str(value), 16)
+            case "string":
+                type = "char *"
+                value = '\"' + value + '\"'
+            case _:
+                type = integer_types[type]
+                value = int(str(value), 16)
+        return type, str(value)
+
+    def is_symbol_float(self, symbol, program) -> bool:
+        listing = program.getListing()
+        addr = symbol.getAddress()
+        data = listing.getDataAt(addr)
+        type = data.getDataType().getName()
+        if not ("undefined" in type or "float" in type or "double" in type):
+            return False
+        for reference in symbol.getReferences():
+            addr = reference.getFromAddress()
+            code_unit = listing.getCodeUnitContaining(addr)
+            if code_unit is None:
+                continue
+            mnemonic = code_unit.getMnemonicString()
+            if mnemonic in floating_point_instructions:
+                return True
+        return False
 
     def get_headers_from_ghidra(self, headers, program):
         data_type_manager = program.getDataTypeManager()
@@ -78,7 +183,7 @@ class PostProcessor:
             if header in libc:
                 headers.add(header)
         return headers
-    
+
     def get_headers_from_functions(self, headers, decompiled_funcs):
         for func in decompiled_funcs:
             variable_declarations = func.getC().split("{")[1].split(";")
@@ -91,14 +196,14 @@ class PostProcessor:
                 variable_declaration = variable_declarations[declarationID].split()
 
         return headers
-        
+
     def write_headers(self, file, program, decompiled_funcs):
         headers = set()
         headers = self.get_headers_from_ghidra(headers, program)
         headers = self.get_headers_from_functions(headers, decompiled_funcs)
 
         for header in headers:
-            if header == None:
+            if header is None:
                 continue
             file.write(f"#include<{header}>\n")
         file.write("\n")
@@ -108,16 +213,16 @@ class PostProcessor:
             if funcs[index].getName() == "main":
                 continue
             func_signature = func.getSignature()
-            for key in undefined_types.keys():
-                func_signature = func_signature.replace(key, undefined_types[key])
+            for key in integer_types.keys():
+                func_signature = func_signature.replace(key, integer_types[key])
             for key in utypes.keys():
                 func_signature = func_signature.replace(key, utypes[key])
             file.write(func_signature + "\n")
 
         for func in decompiled_funcs:
             func_code = func.getC()
-            for key in undefined_types.keys():
-                func_code = func_code.replace(key, undefined_types[key])
+            for key in integer_types.keys():
+                func_code = func_code.replace(key, integer_types[key])
             for key in utypes.keys():
                 func_code = func_code.replace(key, utypes[key])
             file.write(func_code)

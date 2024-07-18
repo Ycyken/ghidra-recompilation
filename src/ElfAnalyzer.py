ac2a7fd5eda5ecb67e2df884565d127f5026ca06
Gleb Nasretdinov <135718038+Ycyken@users.noreply.github.com>
2024-07-18 15:18:28 +0300

    feat: add global variables definition and floats detection

--- src/ElfAnalyzer.py
+++ src/ElfAnalyzer.py
@@ -6,10 +6,14 @@ class ElfAnalyzer:
         elf = ELFFile(open(elfpath, 'rb'))
         self.elf = elf
 
-    def is_function_inside_section(self, func_address: int, image_base: int, section_name: str) -> bool:
+    def get_section_addresses(self, section_name: str) -> tuple[int, int]:
         section = self.elf.get_section_by_name(section_name)
-        start_address = int(section['sh_addr']) + image_base
-        end_address = start_address + int(section['sh_size']) + image_base
+        start_address = int(section['sh_addr'])
+        end_address = start_address + int(section['sh_size'])
+        return start_address, end_address
+
+    def is_function_inside_section(self, func_address: int, image_base: int, section_name: str) -> bool:
+        start_address, end_address = tuple(map (lambda x : x + image_base, self.get_section_addresses(section_name)))
         return start_address <= func_address < end_address
 
     @staticmethod

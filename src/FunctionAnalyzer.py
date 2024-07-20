4c63ef5cd8349eb14f6a49ec65863d7cb5610a6b
Kostya <koreshin67@gmail.com>
2024-07-21 01:00:45 +0300

    ref+feat: reorganize crt and jumpers detection+add new method to decompile code

---
+++ src/FunctionAnalyzer.py
@@ -0,0 +1,22 @@
+class FunctionAnalyzer:
+   def __init__(self, addr_set, listing):
+      self.code_units = listing.getCodeUnits(addr_set, True)
+      self.start_address = int(str(addr_set.getMinAddress()), 16)
+      self.end_address = int(str(addr_set.getMaxAddress()), 16)
+
+   def is_function_incorrect(self):
+      for id, code_unit in enumerate(self.code_units):
+         if str(code_unit) != "ENDBR64" and id == 0:
+            return True
+         
+         elif str(code_unit) == "HLT":
+            return True
+         
+         elif str(code_unit.getMnemonicString()) != "JMP":
+            continue
+
+         destination_address = int(str(code_unit.getPrimaryReference(0).getToAddress()), 16)
+         if self.start_address > destination_address or destination_address > self.end_address:
+            return True
+
+      return False

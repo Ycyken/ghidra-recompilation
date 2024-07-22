392543f537acea2b6eb57860bf34040dfb7dd8e3
Gleb Nasretdinov <gleb.romashkov@gmail.com>
2024-07-23 00:26:11 +0300

    Ref: remove redundant args in Postprocessor methods, add elf file closing

--- src/ElfAnalyzer.py
+++ src/ElfAnalyzer.py
@@ -1,10 +1,27 @@
+import logging
+
 from elftools.elf.elffile import ELFFile
 
 
 class ElfAnalyzer:
+
     def __init__(self, elfpath: str):
-        elf = ELFFile(open(elfpath, 'rb'))
-        self.elf = elf
+        self.elfpath = elfpath
+
+    def __enter__(self):
+        self.open_elf()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close_elf()
+        if exc_type is not None:
+            logging.exception(exc_val)
+
+    def open_elf(self):
+        self.elf = ELFFile(open(self.elfpath, 'rb'))
+
+    def close_elf(self):
+        self.elf.close()
 
     def get_section_addresses(self, section_name: str) -> tuple[int, int]:
         section = self.elf.get_section_by_name(section_name)
@@ -13,5 +30,5 @@ class ElfAnalyzer:
         return start_address, end_address
 
     def is_function_inside_section(self, func_address: int, image_base: int, section_name: str) -> bool:
-        start_address, end_address = tuple(map (lambda x : x + image_base, self.get_section_addresses(section_name)))
+        start_address, end_address = tuple(map(lambda x: x + image_base, self.get_section_addresses(section_name)))
         return start_address <= func_address < end_address

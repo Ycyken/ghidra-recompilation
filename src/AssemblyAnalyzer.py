76eb8892a87843adef1bf1604ec85cb85bd1d427
Gleb Nasretdinov <gleb.romashkov@gmail.com>
2024-07-26 18:11:31 +0300

    Fix: add reference None check in JMP outside function check -for example, in JMP *REGISTER instruction

--- src/AssemblyAnalyzer.py
+++ src/AssemblyAnalyzer.py
@@ -1,23 +1,23 @@
 class AssemblyAnalyzer:
-   def __init__(self, addr_set, listing):
-      self.code_units = listing.getCodeUnits(addr_set, False)
-      self.start_address = int(str(addr_set.getMinAddress()), 16)
-      self.end_address = int(str(addr_set.getMaxAddress()), 16)
+    def __init__(self, addr_set, listing):
+        self.code_units = listing.getCodeUnits(addr_set, False)
+        self.start_address = int(str(addr_set.getMinAddress()), 16)
+        self.end_address = int(str(addr_set.getMaxAddress()), 16)
 
-   def is_function_nonuser(self):
-      for code_unit in self.code_units:
-         if code_unit.getMnemonicString() == "HLT":
-            return True
-         
-         elif code_unit.getMnemonicString() != "JMP":
-            continue
+    def is_function_nonuser(self):
+        for code_unit in self.code_units:
+            if code_unit.getMnemonicString() == "HLT":
+                return True
 
-         primary_reference = code_unit.getPrimaryReference(0)
-         if primary_reference == None:
-            continue
+            elif code_unit.getMnemonicString() != "JMP":
+                continue
 
-         destination_address = int(str(primary_reference.getToAddress()), 16)
-         if self.start_address > destination_address or destination_address > self.end_address:
-            return True
+            primary_reference = code_unit.getPrimaryReference(0)
+            if primary_reference is None:
+                continue
 
-      return False
+            destination_address = int(str(primary_reference.getToAddress()), 16)
+            if self.start_address > destination_address or destination_address > self.end_address:
+                return True
+
+            return False

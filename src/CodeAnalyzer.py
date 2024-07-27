b3eca2ef9e726a35c345e3c423235b1ac7bb839d
Kostya <koreshin67@gmail.com>
2024-07-27 14:15:06 +0300

    ref: change some variables names

--- src/CodeAnalyzer.py
+++ src/CodeAnalyzer.py
@@ -1,12 +1,12 @@
 stack_protectors = ["__stack_chk_fail", "___stack_chk_guard", "in_FS_OFFSET"]
 
 class CodeAnalyzer:
-    def __init__(self, signature, func_code):
+    def __init__(self, signature: str, func_code: str):
         self.func_code = func_code
         self.signature = signature
         self.types_of_variables = {}
         
-    def get_variable_name(self, variable):
+    def get_variable_name(self, variable: str) -> str:
         if "*" in variable:
                 variable = variable[variable.rfind("*")+1:]
         if "[" in variable:
@@ -24,22 +24,23 @@ class CodeAnalyzer:
             self.types_of_variables[variable] = type
         return self.types_of_variables
 
-    def remove_warnings(self, lines, id, warning_in_load, warning_in_store):
-        lvalue, rvalue = lines[id].split(" = ")
+    def get_line_without_warnings(self, line: str, warning_in_load: bool, warning_in_store: bool) -> str:
+        lvalue, rvalue = line.split(" = ")
         variable = self.get_variable_name(lvalue.strip())
         if warning_in_load:
             if rvalue[1] == "(":
-                lines[id] = lvalue + " = " + rvalue[0] + f"({self.types_of_variables[variable]} *)" + rvalue[rvalue.index(")")+1:]
+                line = lvalue + " = " + rvalue[0] + f"({self.types_of_variables[variable]} *)" + rvalue[rvalue.index(")")+1:]
             else:
-                lines[id] = lvalue + " = " + rvalue[0] + f"({self.types_of_variables[variable]} *)" + rvalue[1:]
+                line = lvalue + " = " + rvalue[0] + f"({self.types_of_variables[variable]} *)" + rvalue[1:]
         
         if warning_in_store:
             if lvalue[1] == "(":
-                lines[id] = lvalue[0] + f"({self.types_of_variables[variable]} *)" + lvalue[lvalue.index(")")+1:] + " = " + rvalue
+                line = lvalue[0] + f"({self.types_of_variables[variable]} *)" + lvalue[lvalue.index(")")+1:] + " = " + rvalue
             else:
-                lines[id] = lvalue[0] + f"({self.types_of_variables[variable]} *)" + lvalue[1:] + " = " + rvalue
+                line = lvalue[0] + f"({self.types_of_variables[variable]} *)" + lvalue[1:] + " = " + rvalue
+        return line
 
-    def get_code_without_warnings(self):
+    def get_code_without_warnings(self) -> str:
         warning_in_load = False
         warning_in_store = False
         need_to_delete_brace = False
@@ -47,19 +48,19 @@ class CodeAnalyzer:
         self.types_of_variables = self.get_variables_types_from_signature()
 
         lines = self.func_code.split("\n")
-        declarationID = 0
-        while "{" not in lines[declarationID]:
-            declarationID += 1
-        declarationID += 1
+        lineID = 0
+        while "{" not in lines[lineID]:
+            lineID += 1
+        lineID += 1
 
-        declaration = lines[declarationID][:lines[declarationID].find(" [")].split()
-        while len(declaration) == 2 and declaration[0] not in ["return", "do"]:
-            type, variable = declaration[0], self.get_variable_name(declaration[1])
+        line = lines[lineID][:lines[lineID].find(" [")].split()
+        while len(line) == 2 and line[0] not in ["return", "do"]:
+            type, variable = line[0], self.get_variable_name(line[1])
             self.types_of_variables[variable] = type
-            declarationID += 1
-            declaration = lines[declarationID][:lines[declarationID].find(" [")].split()
+            lineID += 1
+            line = lines[lineID][:lines[lineID].find(" [")].split()
         
-        for id in range(declarationID, len(lines)):
+        for id in range(lineID, len(lines)):
             if "WARNING: Load size is inaccurate" in lines[id]:
                 warning_in_load = True
                 continue
@@ -69,7 +70,7 @@ class CodeAnalyzer:
                 continue
 
             elif warning_in_load or warning_in_store:
-                self.remove_warnings(lines, id, warning_in_load, warning_in_store)
+                lines[id] = self.get_line_without_warnings(lines[id], warning_in_load, warning_in_store)
                 warning_in_load, warning_in_store = False, False
                 continue
 
